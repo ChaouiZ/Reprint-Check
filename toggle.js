8dd903cb53d56f01b8f3f3af48305384fac1cc3c
ChaouiZ <106296433+ChaouiZ@users.noreply.github.com>
2024-08-06 15:02:17 -0500

    Updated a lot lol

--- toggle.js
+++ toggle.js
@@ -1,12 +1,20 @@
 const reprintCheck = document.getElementById("reprint-check-toggle");
-const reprintCoveredCheckDiv = document.getElementById("reprint-covered-check");
+const upsResolutionCheck = document.querySelector("#ups-claim-check-toggle");
+const reprintCoveredCheckDiv = document.getElementById("ups-claim-check");
+const upsClaimInput = document.querySelector("#ups-claim-input");
+const brianResolutionToggle = document.querySelector(
+  "#brian-resolution-check-toggle"
+);
+
+const brianInputDiv = document.querySelector(".brian-coupon-check-input-div");
+
 const testCheck = document.getElementById("test-check");
 const reprintCoveredCheckToggle = document.getElementById(
   "reprint-covered-check-toggle"
 );
 const textInputDiv = document.getElementById("text-input-div");
-const textInput = document.getElementById("text-input");
-const brianCouponDiv = document.querySelector(".brian-coupon-check");
+const orderNumberInput = document.querySelector("#text-input-order-num");
+const brianCouponDiv = document.querySelector("#brian-coupon-check");
 
 function updateDisplay() {
   if (reprintCheck.checked) {
@@ -17,12 +25,28 @@ function updateDisplay() {
     reprintCoveredCheckDiv.style.display = "none";
     textInputDiv.style.display = "none";
     brianCouponDiv.style.display = "none";
-    textInput.value = "";
-    reprintCoveredCheckToggle.checked = false;
+    orderNumberInput.value = "";
+    reprintCoveredCheckToggle.value = "";
+    upsResolutionCheck.checked = false;
+    brianResolutionToggle.checked = false;
+  }
+
+  if (upsResolutionCheck.checked) {
+    upsClaimInput.style.display = "flex";
+  } else {
+    upsClaimInput.style.display = "none";
+  }
+
+  if (brianResolutionToggle.checked) {
+    brianInputDiv.style.display = "flex";
+  } else {
+    brianInputDiv.style.display = "none";
   }
 }
 
 console.log(reprintCheck, "\n", reprintCoveredCheckDiv, "\n,", testCheck);
 
 reprintCheck.addEventListener("change", updateDisplay);
+upsResolutionCheck.addEventListener("change", updateDisplay);
+brianResolutionToggle.addEventListener("change", updateDisplay);
 window.addEventListener("load", updateDisplay);
